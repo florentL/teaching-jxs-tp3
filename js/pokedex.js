f9da3b682be24c8d601c24933d18a79835d0d0c8
florentL <florent.le.b@hotmail.fr>
2016-04-07 00:23:12 +0200

    Added sprites / Retrieve all pokemons from API v2

--- js/pokedex.js
+++ js/pokedex.js
@@ -7,42 +7,54 @@ pokeApp.config(['$resourceProvider', function($resourceProvider) {
 var pokeApiUrl = "http://pokeapi.co/"
 
 pokeApp.controller('search', ['$scope','$log','$http','apiService', function($scope, $log, $http, apiService) {
-	$scope.$log = $log;
-  $scope.pokemon = [
-  {"id": 1,"nom": 'Pikachu'},
-  {"id": 2,"nom": 'Salamèche'},
-  {"id": 3,"nom": 'Carapuce'},
-  ];
+  $scope.$log = $log;
   
   apiService.getNom(function(pokemons) {
 	$scope.api = pokemons;
   });
-  
 
-  $scope.displayInfos = function(uri) {
+  $scope.displayInfos = function(uri, id) {
+	  
+	  if (typeof (uri) === "undefined") {
+		  uri = pokeApiUrl+"api/v2/pokemon/"+id+"/";
+	  }
+	  
 	  apiService.getSkills(uri,function(response) {
         
 		$scope.infos = [
+		  {"nom": "id", "value": response.id},
           {"nom": "name", "value": response.name},
           {"nom": "weight", "value": response.weight},
           {"nom": "height", "value": response.height},
           {"nom": "experience", "value": response.base_experience}
        ];
+		$scope.sprite = response.sprites.front_default;
 	  });
   }
   
+  $scope.$watch('selectPokemon', function(value) {
+	  if (typeof (value) !== "undefined") {
+		  $scope.displayInfos(value, '');
+	  }
+  })
+  
+  // permet de filtrer uniquement les noms commencant par le pattern entré
   $scope.customComparator = function(actual, expected){
     return (actual.toString().toLowerCase().indexOf(expected.toString().toLowerCase()) === 0);
   }
 }]).
+
 factory('apiService', ['$resource', '$log', function(http, logger) {
 
 	  return {
 		getNom : function(callback) {
 //			http(pokeApiUrl+'api/v1/pokedex/1/').get().$promise.then(function successCallback(response) {
-
+			
+			// need this because API v2 limit is 20 (by default)
+			var nbPokemons = 811;
+			
             // call API v2
-			http(pokeApiUrl+'api/v2/pokemon/').get().$promise.then(function successCallback(response) {
+			http(pokeApiUrl+'api/v2/pokemon/?limit='+nbPokemons).get().$promise.then(function successCallback(response) {
 				
 				var pokemons = response.results.map(function(e) {
                     return e;
@@ -55,6 +67,7 @@ factory('apiService', ['$resource', '$log', function(http, logger) {
 			},
 		getSkills : function(uri,callback) {
 //			http(':uri',{uri:'@uri'}).get({uri:uri}).$promise.then(function successCallback(response) {
+			
 			http(uri).get().$promise.then(function successCallback(response) {
                 callback(response);
 			  }, function errorCallback(response) {
